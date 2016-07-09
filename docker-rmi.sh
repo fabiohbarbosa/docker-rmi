3651432dd6aa8aebdb1aeac4125cc182a47f166d
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2016-07-09 11:10:31 -0300

    shell script

---
+++ docker-rmi.sh
@@ -0,0 +1,21 @@
+function isEmpty
+{
+  if [ -z "$1" ]
+  then
+    echo "Not found untagged images"
+    exit 0
+  fi
+}
+
+IMAGES_ID=$(docker images | grep "^<none>" | awk '{print $3}')
+
+isEmpty $IMAGES_ID
+
+for IMG in $IMAGES_ID
+do
+  CONTAINER_ID=$(docker ps -a | grep "$IMG" | awk '{print $1}')
+  docker stop $CONTAINER_ID
+  docker rm $CONTAINER_ID
+done
+
+docker rmi $IMAGES_ID
